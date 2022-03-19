33e0425f4163a119624435320a065a146d948bcf
Arun Chithambaram <arun.chithambaram@newglobe.education>
2022-03-19 14:14:31 +0530

    Updates to use ES modules

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import App from "./App.svelte";
+import App from './App.svelte';
 
-const app = new App({ target: document.querySelector("#app") });
+const app = new App({ target: document.querySelector('#app') });
 
 export default app;
